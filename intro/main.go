d094ac17f97deaf14b6f0d76397d28750db22e68
Christian Wansart <christian.wansart@openknowledge.de>
2020-08-04 21:36:20 +0200

    add context html

--- intro/main.go
+++ intro/main.go
@@ -5,15 +5,37 @@ import (
 	"os"
 )
 
-func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
+type Test struct {
+	HTML     string
+	SafeHTML template.HTML
+	Title    string
+	Path     string
+	Dog      Dog
+	Map      map[string]string
+}
 
-	data := struct {
-		Name string
-	}{"Christian Wansart"}
+type Dog struct {
+	Name string
+	Age  int
+}
+
+func main() {
+t, err := template.ParseFiles("context.gohtml")
+if err != nil {
+	panic(err)
+}
+
+	data := Test{
+		HTML:     "<h1>A header</h1>",
+		SafeHTML: template.HTML("<h1>A Safe header</h1>"),
+		Title:    "Backslash! An in depth look ath the\"\\\" character",
+		Path:     "/dashboard/settings",
+		Dog:      Dog{"Malou", 14},
+		Map: map[string]string{
+			"key":       "value",
+			"other_key": "other_value",
+		},
+	}
 
 	err = t.Execute(os.Stdout, data)
 	if err != nil {
